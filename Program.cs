d07343fe4a76fdc6c1b8f7f23b02f3f6213d484e
giselle <gisellelago25994@gmail.com>
2019-12-09 19:54:18 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System;
+
+class Solution
+{
+
+    // Complete the saveThePrisoner function below.
+    static int saveThePrisoner(int n, int m, int s)
+    {
+        var resFinal = m + s - 1; 
+        if (m >= n)
+        {
+           while (m > n)
+            {
+                m -= n;
+            }
+        }
+        if (resFinal >= n)
+        {
+            while (resFinal > n)
+            {
+                resFinal -= n;
+            }
+        }
+        
+        return resFinal;
+       
+    }
+
+    static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] nms = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nms[0]);
+
+            int m = Convert.ToInt32(nms[1]);
+
+            int s = Convert.ToInt32(nms[2]);
+
+            int result = saveThePrisoner(n, m, s);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+    }
+}
